ba40a1a318655e8a1d41d143e59efd544b4da33d
XimenaRubio <A01378326@itesm.mx>
2023-09-06 19:08:46 -0600

    /modelo

--- servidor.py
+++ servidor.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, jsonify
 import numpy as np
 from joblib import load
 import os
@@ -14,12 +14,20 @@ def formulario():
     return render_template('pagina1.html')
 
 #Envio de datos a traves de JSON
-@servidorWeb.route('/modelo',methods=['GET'])
+@servidorWeb.route('/modelo',methods=['POST'])
 def modeloPrediccion():
         #Procesar los datos de entrada
         contenido= request.json
         print(contenido)
-        return jsonify({'resultado':"Hola"})
+        datosEntrada = np.array([
+               .88,0,2.6,.098,25,67,0.9968,
+               contenido['pH'],
+               contenido['sulphates'],
+               contenido['alcohol']
+        ])
+        #Utilizar modelol
+        resultado=dt.predict(datosEntrada.reshape(1,-1))
+        return jsonify({'resultado':str(resultado[0])})
 
 if __name__ == '__main__':
         servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
